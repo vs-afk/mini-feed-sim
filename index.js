ccf25c4b2d84a735dbe43fedda9e3e247a3c9db5
Vince Saenz <vincesaenz89@gmail.com>
2026-03-11 22:04:22 -0500

    work

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ let postArray = [];
 
 function createPost(username, content) {
     let post = {
-        id: 1,
+        id: 0,
         username: username,
         content: content,
         likes: 0,
         timestamp: 12342322
     };
-    
+    if (postArray.length >= 0 ) {
+        post.id = post.id + 1
+    }
     postArray.push(post)
 }
 
 createPost("vice", "playing golf on Friday!")
-
+createPost("ally", "Taking the cats for a walk!")
 
 
 console.log(postArray)
