bd3068a6aee8effeef55e33bdb493e964a4012c7
Vince Saenz <vincesaenz89@gmail.com>
2026-03-14 12:05:50 -0500

    functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let postArray = [];
 
 function createPost(username, content) {
-    let post = {
+        let post = {
         id: 0,
         username: username,
         content: content,
@@ -16,6 +16,25 @@ function createPost(username, content) {
     post.timestamp = post.id * 2
 
     postArray.push(post);
+
+}
+
+
+function likePost(x) {
+    for (let i = 0; i <postArray.length; i++ ) {
+        if (postArray[i].id === x) {
+            postArray[i].likes++;
+        }
+    }
+}
+
+function sortByNewest() {
+    for (let i = 0; i < postArray.length; i++)
+        
+}
+
+function getFeed() {
+    console.log(postArray)
 }
 
 createPost("vice", "playing golf on Friday!")
@@ -23,17 +42,7 @@ createPost("ally", "Taking the cats for a walk!")
 createPost("sookie","Where's my cricket!")
 createPost("Nadja", "Lets watch Intersteller!")
 
-
-
-
-function likePost(x) {
-    for (let n = 0;n < postArray.length; n++ ) {
-        if (n === x) {
-            post.id =+1
-        };
-    }
-}
-
 likePost(3)
+likePost(2)
 
-console.log(postArray)
+getFeed()
